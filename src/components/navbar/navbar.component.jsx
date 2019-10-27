d5269c909b93c44e34c5fa7e5854b0e95ebfc45f
vanshika <anandvanshika8@gmail.com>
2019-10-27 10:37:48 +0530

    added Navbar.Collapse in navigate bar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 //import './navbar.styles.scss'
-
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 const Navigatebar = ({changeRoute}) => {
     return (
-        
-            <Nav fill variant='tabs' defaultActiveKey='search' onSelect = {changeRoute}>
-                <Nav.Link eventKey="search" > Search </Nav.Link>
-                <Nav.Link eventKey="compare">Compare</Nav.Link>
-            </Nav>
+        <Navbar bg="light" expang="lg">
+            <Navbar.Collapse className='justify-content-end'>
+
+                <Nav fill variant='tabs' defaultActiveKey='search' onSelect = {changeRoute}>
+                    <Nav.Link eventKey="search" > Search </Nav.Link>
+                    <Nav.Link eventKey="compare">Compare</Nav.Link>
+                </Nav>
+
+            </Navbar.Collapse>
+        </Navbar>
         
     )
 }
